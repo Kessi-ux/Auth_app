d3eb900cd09ec3f2cf816fb53f60774f524a97eb
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2024-02-13 07:41:20 -0800

    add creatresetpassword token function

--- models/userModel.js
+++ models/userModel.js
@@ -25,6 +25,16 @@ userSchema.pre('save', async function(next) {
     next();
   });
   
+  userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    
+    //encrypt reset token
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetTokenExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
+  
   userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
   };
